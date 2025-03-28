4e3b2eedbfdc1eb50ef7bbcbc98a632a2132318c
Bayryam Dermendzhiev <bairqm.dermendzhiev123@gmail.com>
2025-03-28 23:56:16 +0200

    init be

---
+++ a-new-hope-be/src/main/java/com/hackaton/project/anewhopebe/ANewHopeBeApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.project.anewhopebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ANewHopeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ANewHopeBeApplication.class, args);
+    }
+
+}
